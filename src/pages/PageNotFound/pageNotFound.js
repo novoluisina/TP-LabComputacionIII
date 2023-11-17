de1257da7682af74711ea49378492ed68553480d
cinti.av <cin899a@gmail.com>
2023-11-17 01:47:56 -0300

    cambios

--- src/pages/PageNotFound/pageNotFound.js
+++ src/pages/PageNotFound/pageNotFound.js
@@ -1,19 +1,18 @@
-import React from "react";
-import { Button } from "react-bootstrap";
-import { useNavigate } from "react-router";
+import React from 'react'
+import { useNavigate } from 'react-router'
 
 const PageNotFound = () => {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
   const backMainPageHandler = () => {
-    navigate("/");
-  };
+    navigate('/')
+  }
   return (
     <div>
-      <h2 >La pagina solicitada no fue encontrada</h2>
-      <Button onClick={backMainPageHandler}>Volver a recetas</Button>
+      <h2>La pagina solicitada no fue encontrada</h2>
+      <button onClick={backMainPageHandler}>Volver a recetas</button>
     </div>
-  );
-};
+  )
+}
 
-export default PageNotFound;
+export default PageNotFound
